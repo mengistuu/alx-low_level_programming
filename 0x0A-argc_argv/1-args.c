10a4d984d9592007fc6a57194956fe6988502697
Mengistuu <mathiangmalang@gmail.comgit config --global user.emailgit config --global user.email mathiangmalang@gmail.com>
2022-04-02 00:18:31 +0300

    yolo

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-/**
- * isInteger - checks if s is an integer
- * @s: string to check
- * Return: 0 or 1
- */
-
-int isInteger(const char *s)
-{
-int i = 0;
-while (s[i] != '\0')
-{
-	if (s[i] < '0' || s[i] > '9')
-		return (0);
-	i++;
-}
-return (1);
-}
 
 /**
- * main - adds positive numbers
+ * main - prints the number of arguments passed into the program
  * @argc: int
  * @argv: list
  * Return: 0
@@ -27,32 +9,7 @@ return (1);
 
 int main(int argc, char const *argv[])
 {
-int i = 0, coinUsed = 0, coin = 0;
-int coins[] = {25, 10, 5, 2, 1};
-
-if (argc != 2)
-{
-	printf("Error\n");
-	return (1);
-}
-if (isInteger(argv[1]))
-{
-	i = atoi(argv[1]);
-	while (i > 0 && coin <= 4)
-	{
-		if (i >= coins[coin])
-		{
-			i -= coins[coin];
-			coinUsed++;
-		}
-		else
-		{
-			coin++;
-		}
-	}
-}
-
-printf("%i\n", coinUsed);
-
+(void)argv;
+printf("%i\n", argc - 1);
 return (0);
 }
