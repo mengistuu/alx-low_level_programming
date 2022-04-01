a55eeb6935d8dbaa7d91072177dcde99886a4728
Mengistuu <mathiangmalang@gmail.comgit config --global user.emailgit config --global user.email mathiangmalang@gmail.com>
2022-04-02 00:21:01 +0300

    yolo

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - prints the number of arguments passed into the program
- * @argc: int
- * @argv: list
+ * main - prints the number of args passed into it
+ * @argc: arg count
+ * @argv: args passed as an array of strs
  * Return: 0
  */
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-(void)argv;
-printf("%i\n", argc - 1);
-return (0);
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
 }
